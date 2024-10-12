6f1f4e8235ef4a22e0ba68e832d3c7af8eccb121
Yaroslav K <69163097+Yaroslav-K-V@users.noreply.github.com>
2024-10-12 20:33:59 +0300

    Update site.py

--- site.py
+++ site.py
@@ -1,11 +1,10 @@
-import threading
-import time
 from flask import Flask
+import threading
 import subprocess
 
 app = Flask(__name__)
 
-# Запускаємо основний бот у окремому потоці
+# Запускаємо бота в окремому потоці
 def run_bot():
     subprocess.Popen(["python", "main_bot.py"])
 
@@ -14,9 +13,9 @@ def home():
     return "Bot is running!", 200
 
 if __name__ == "__main__":
-    # Запускаємо бота в окремому потоці, щоб він працював паралельно з вебсервером
+    # Запускаємо бота в окремому потоці
     bot_thread = threading.Thread(target=run_bot)
     bot_thread.start()
-    
-    # Запускаємо вебсервер
+
+    # Запускаємо Flask-сервер
     app.run(host="0.0.0.0", port=5000)
